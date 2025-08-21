0d1f1e1955393182337f4bb4401b0609fc24d512
0ttey <locottey@gmail.com>
2025-08-21 09:18:17 +0100

    commit message

---
+++ airbnbfinal.js
@@ -0,0 +1,6 @@
+let loginButton = document.querySelector("#login-button")
+// function to open login page
+function login(){
+    window.location="login_page.html"
+}
+loginButton.addEventListener("click", login)
